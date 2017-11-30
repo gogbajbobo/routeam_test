90438765bad39e84fdf2e28ab8b5a9fd5fac1d31
gogbajbobo <grigoriev.maxim@gmail.com>
2017-11-30 15:52:46 +0300

    generate events data

--- routeam_test/DataController.m
+++ routeam_test/DataController.m
@@ -10,6 +10,8 @@
 
 #import <CoreData/CoreData.h>
 #import <UIKit/UIKit.h>
+#import "DataModel/DataModel.h"
+
 
 @interface DataController()
 
@@ -106,7 +108,7 @@
 - (void)documentReady:(UIManagedDocument *)document {
     
     self.document = document;
-    [DataController requestEventsData];
+    [DataController checkEventsData];
     
 }
 
@@ -117,9 +119,27 @@
 
 #pragma mark - class methods
 
++ (NSManagedObjectContext *)context {
+    return [DataController sharedController].document.managedObjectContext;
+}
+
++ (void)checkEventsData {
+    
+    NSFetchRequest *request = [NSFetchRequest fetchRequestWithEntityName:NSStringFromClass([Event class])];
+    NSError *error = nil;
+    
+    NSUInteger eventsCount = [[self context] countForFetchRequest:request
+                                                            error:&error];
+    
+    if (!eventsCount) {
+        [self requestEventsData];
+    }
+    
+}
+
 + (void)requestEventsData {
     
-    [self getDataWithCompletionHandler:^(BOOL success, NSDictionary *data) {
+    [self getEventsDataWithCompletionHandler:^(BOOL success, NSArray <NSDictionary *> *data) {
         
         if (!success) return;
         [self handleEventsData:data];
@@ -128,22 +148,83 @@
     
 }
 
-+ (void)getDataWithCompletionHandler:(void (^)(BOOL success, NSDictionary *data))completionHandler {
++ (void)getEventsDataWithCompletionHandler:(void (^)(BOOL success, NSArray <NSDictionary *> *data))completionHandler {
     
-    NSDictionary *returnData = [self eventsData];
+    NSArray <NSDictionary *> *returnData = [self eventsData];
     BOOL success = YES;
     
     completionHandler(success, returnData);
     
 }
 
-+ (NSDictionary *)eventsData {
++ (NSArray <NSDictionary *> *)eventsData {
     
-    return @{};
+    NSMutableArray <NSDictionary *> *result = @[].mutableCopy;
+    
+    for (NSUInteger i = 0; i < 10; i++) {
+        [result addObject:[self eventWithId:i]];
+    }
+    
+    return result;
     
 }
 
-+ (void)handleEventsData:(NSDictionary *)eventsData {
++ (NSDictionary *)eventWithId:(NSUInteger)eventId {
+    
+    NSString *idString = @(eventId).stringValue;
+    NSDate *startDate = [NSDate dateWithTimeIntervalSinceNow:-(eventId * 24 * 3600)];
+    NSDate *finishDate = [NSDate dateWithTimeIntervalSinceNow:-(eventId * 12 * 3600)];
+    NSNumber *completion = @(10 * eventId);
+    EventType type = 3 % (eventId + 1);
+
+    NSDictionary *event = @{@"id": @(eventId),
+                            @"name": [@"Event " stringByAppendingString:idString],
+                            @"startDate": startDate,
+                            @"finishDate": finishDate,
+                            @"info": [NSString stringWithFormat:@"Event %@ info", idString],
+                            @"completion": completion,
+                            @"type": @(type)
+                            };
+    
+    return event;
+    
+}
+
++ (NSArray *)participantsWithEventId:(NSUInteger)eventId {
+    return nil;
+}
+
++ (NSDictionary *)participantWithId:(NSUInteger)participantId {
+    return nil;
+}
+
++ (void)handleEventsData:(NSArray <NSDictionary *> *)eventsData {
+    
+    [eventsData enumerateObjectsUsingBlock:^(NSDictionary * _Nonnull event, NSUInteger idx, BOOL * _Nonnull stop) {
+        [self handleEvent:event];
+    }];
+    
+}
+
++ (void)handleEvent:(NSDictionary *)event {
+
+    NSEntityDescription *entity = [NSEntityDescription entityForName:NSStringFromClass([Event class])
+                                              inManagedObjectContext:[self context]];
+    
+    NSArray *attributes = entity.attributesByName.allKeys;
+
+    for (NSString *attribute in attributes) {
+        
+        if (!event[attribute]) continue;
+        
+        NSAttributeDescription *attributeDescription = entity.attributesByName[attribute];
+        NSString *attributeClass = attributeDescription.attributeValueClassName;
+        
+        NSLog(@"attributeClass %@", attributeClass);
+        NSLog(@"event[attribute] %@", event[attribute]);
+        
+        
+    }
     
 }
 
