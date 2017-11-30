5aa33fb40debe64ed8b89239395c4219d83cf4c9
gogbajbobo <grigoriev.maxim@gmail.com>
2017-11-30 11:42:41 +0300

    add DataController class

---
+++ routeam_test/DataController.h
@@ -0,0 +1,13 @@
+//
+//  DataController.h
+//  routeam_test
+//
+//  Created by Maxim Grigoriev on 30/11/2017.
+//  Copyright © 2017 Maxim Grigoriev. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface DataController : NSObject
+
+@end
---
+++ routeam_test/DataController.m
@@ -0,0 +1,13 @@
+//
+//  DataController.m
+//  routeam_test
+//
+//  Created by Maxim Grigoriev on 30/11/2017.
+//  Copyright © 2017 Maxim Grigoriev. All rights reserved.
+//
+
+#import "DataController.h"
+
+@implementation DataController
+
+@end
