802bac61fa9022b0a12e2f97762588dc9b7e7a1c
gogbajbobo <grigoriev.maxim@gmail.com>
2017-11-30 11:45:06 +0300

    handle initialization of UIManagedDocument

--- routeam_test/DataController.m
+++ routeam_test/DataController.m
@@ -8,6 +8,17 @@
 
 #import "DataController.h"
 
+#import <CoreData/CoreData.h>
+#import <UIKit/UIKit.h>
+
+@interface DataController()
+
+@property (nonatomic, strong) UIManagedDocument *document;
+
+
+@end;
+
+
 @implementation DataController
 
 
@@ -24,6 +35,83 @@
     
     return _sharedInstance;
 
+}
+
+
+#pragma mark - instance methods
+
+- (instancetype)init {
+    
+    self = [super init];
+    if (self) {
+        [self prepareDocument];
+    }
+    return self;
+    
+}
+
+- (void)prepareDocument {
+    
+    NSFileManager *fileManager = [NSFileManager defaultManager];
+
+    NSURL *documentDirectoryUrl = [fileManager URLsForDirectory:NSDocumentDirectory
+                                                      inDomains:NSUserDomainMask].lastObject;
+    NSURL *documentUrl = [documentDirectoryUrl URLByAppendingPathComponent:@"db_document"];
+    UIManagedDocument *document = [[UIManagedDocument alloc] initWithFileURL:documentUrl];
+    
+    if (![fileManager fileExistsAtPath:document.fileURL.path]) {
+        
+        [self createDocument:document];
+        
+    } else if (document.documentState == UIDocumentStateClosed) {
+        
+        [self openDocument:document];
+        
+    } else if (document.documentState == UIDocumentStateNormal) {
+        
+        [self documentReady:document];
+        
+    }
+    
+}
+
+- (void)createDocument:(UIManagedDocument *)document {
+    
+    [document saveToURL:document.fileURL forSaveOperation:UIDocumentSaveForCreating completionHandler:^(BOOL success) {
+        
+        [self handleHandlerForDocument:document
+                               success:success];
+        
+    }];
+    
+}
+
+- (void)openDocument:(UIManagedDocument *)document {
+    
+    [document openWithCompletionHandler:^(BOOL success) {
+        
+        [self handleHandlerForDocument:document
+                               success:success];
+        
+    }];
+    
+}
+
+- (void)handleHandlerForDocument:(UIManagedDocument *)document success:(BOOL)success {
+    
+    (success) ? [self documentReady:document] : [self documentNotReady:document];
+    
+}
+
+- (void)documentReady:(UIManagedDocument *)document {
+    
+    self.document = document;
+    [DataController requestEventsData];
+    
+}
+
+- (void)documentNotReady:(UIManagedDocument *)document {
+    
 }
 
 
@@ -58,4 +146,6 @@
 + (void)handleEventsData:(NSDictionary *)eventsData {
     
 }
+
+
 @end
