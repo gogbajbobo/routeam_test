96df9325465d3cd74e556f3625d4b7f457c246ae
gogbajbobo <grigoriev.maxim@gmail.com>
2017-12-02 22:03:16 +0300

    add outlets and actions

--- routeam_test/Classes/Views/FilterOptionsVC.m
+++ routeam_test/Classes/Views/FilterOptionsVC.m
@@ -10,9 +10,74 @@
 
 @interface FilterOptionsVC ()
 
+@property (weak, nonatomic) IBOutlet UILabel *typeLabel;
+@property (weak, nonatomic) IBOutlet UILabel *nameLabel;
+@property (weak, nonatomic) IBOutlet UILabel *startDateLabel;
+@property (weak, nonatomic) IBOutlet UILabel *finishDateLabel;
+@property (weak, nonatomic) IBOutlet UILabel *completionMinLabel;
+@property (weak, nonatomic) IBOutlet UILabel *completionMaxLabel;
+@property (weak, nonatomic) IBOutlet UILabel *filteredEventsCountLabel;
+
+@property (weak, nonatomic) IBOutlet UISwitch *typeSwitch;
+@property (weak, nonatomic) IBOutlet UISwitch *nameSwitch;
+@property (weak, nonatomic) IBOutlet UISwitch *startDateSwitch;
+@property (weak, nonatomic) IBOutlet UISwitch *finishDateSwitch;
+@property (weak, nonatomic) IBOutlet UISwitch *completionMinSwitch;
+@property (weak, nonatomic) IBOutlet UISwitch *completionMaxSwitch;
+
+@property (weak, nonatomic) IBOutlet UISegmentedControl *typeSegmentedControl;
+@property (weak, nonatomic) IBOutlet UITextField *nameTextField;
+@property (weak, nonatomic) IBOutlet UIButton *startDateValueButton;
+@property (weak, nonatomic) IBOutlet UIButton *finishDateValueButton;
+@property (weak, nonatomic) IBOutlet UISlider *completionMinSlider;
+@property (weak, nonatomic) IBOutlet UISlider *completionMaxSlider;
+
+
 @end
 
 @implementation FilterOptionsVC
+
+
+#pragma mark - actions
+
+- (IBAction)typeSwitchChanged:(id)sender {
+}
+
+- (IBAction)nameSwitchChanged:(id)sender {
+}
+
+- (IBAction)startDateSwitchChanged:(id)sender {
+}
+
+- (IBAction)finishDateSwitchChanged:(id)sender {
+}
+
+- (IBAction)completionMinSwitchChanged:(id)sender {
+}
+
+- (IBAction)completionMaxSwitchChanged:(id)sender {
+}
+
+- (IBAction)typeValueChanged:(id)sender {
+}
+
+- (IBAction)nameValueChanged:(id)sender {
+}
+
+- (IBAction)startDateFilterPressed:(id)sender {
+}
+
+- (IBAction)finishDateFilterPressed:(id)sender {
+}
+
+- (IBAction)completionMinChanged:(id)sender {
+}
+
+- (IBAction)completionMaxChanged:(id)sender {
+}
+
+
+#pragma mark - view lifecycle
 
 - (void)viewDidLoad {
     
