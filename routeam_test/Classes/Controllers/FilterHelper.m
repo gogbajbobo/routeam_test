f6870ab91f63d42806cdb89c8658c11aebd4d02d
gogbajbobo <grigoriev.maxim@gmail.com>
2017-12-03 10:18:55 +0300

    generate predicate for events filtering

--- routeam_test/Classes/Controllers/FilterHelper.m
+++ routeam_test/Classes/Controllers/FilterHelper.m
@@ -8,10 +8,51 @@
 
 #import "FilterHelper.h"
 
+#import "SettingsController.h"
+
+
 @implementation FilterHelper
 
 + (NSPredicate *)eventsPredicate {
-    return [NSPredicate predicateWithFormat:@"1 == 1"];
+    
+    NSDictionary *filterSettings = [SettingsController currentSettings][FILTER_OPTIONS];
+    NSArray *filterKeys = [SettingsController filterKeys];
+    NSMutableArray *predicates = @[].mutableCopy;
+    
+    for (NSString *filterKey in filterKeys) {
+        
+        NSNumber *filterState = filterSettings[filterKey][FILTER_STATE];
+        if (filterState.boolValue) {
+            [predicates addObject:[self predicateForFilterKey:filterKey]];
+        }
+        
+    }
+    
+    return [NSCompoundPredicate andPredicateWithSubpredicates:predicates];
+    
+}
+
++ (NSPredicate *)predicateForFilterKey:(NSString *)filterKey {
+
+    NSDictionary *filterSettings = [SettingsController currentSettings][FILTER_OPTIONS];
+    id filterValue = filterSettings[filterKey][FILTER_VALUE];
+    
+    if ([filterKey isEqualToString:FILTER_TYPE]) {
+        return [NSPredicate predicateWithFormat:@"type == %@", filterValue];
+    } else if ([filterKey isEqualToString:FILTER_NAME]) {
+        return [NSPredicate predicateWithFormat:@"name == %@", filterValue];
+    } else if ([filterKey isEqualToString:FILTER_STARTDATE]) {
+        return [NSPredicate predicateWithFormat:@"startDate >= %@", filterValue];
+    } else if ([filterKey isEqualToString:FILTER_FINISHDATE]) {
+        return [NSPredicate predicateWithFormat:@"finishDate <= %@", filterValue];
+    } else if ([filterKey isEqualToString:FILTER_COMPLETION_MIN]) {
+        return [NSPredicate predicateWithFormat:@"completion >= %@", filterValue];
+    } else if ([filterKey isEqualToString:FILTER_COMPLETION_MAX]) {
+        return [NSPredicate predicateWithFormat:@"completion <= %@", filterValue];
+    }
+    
+    return nil;
+    
 }
 
 
